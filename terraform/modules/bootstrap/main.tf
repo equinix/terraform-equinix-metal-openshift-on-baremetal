42324e9625255ca0b8b14ebc97cc439e893fdcfd
JR <liveaverage@gmail.com>
2020-05-07 16:46:27 -0400

    Add handling dynamic iPXE scripts

--- terraform/modules/bootstrap/main.tf
+++ terraform/modules/bootstrap/main.tf
@@ -6,16 +6,33 @@ variable "ssh_private_key_path" {}
 variable "project_id" {}
 variable "cf_zone_id" {}
 
+resource "packet_reserved_ip_block" "ip_bootstrap" {
+  project_id = "${var.project_id}"
+  facility = "${var.facility}"
+  quantity = var.node_count
+}
+
 resource "packet_device" "openshift_bootstrap" {
   hostname         = "${format("${var.cluster_name}-${var.facility}-bootstrap-%02d", count.index)}"
   operating_system = "custom_ipxe"
-  ipxe_script_url  = "https://raw.githubusercontent.com/RedHatSI/openshift-packet-deploy/bootstrap-test/ipxe/rhcos-packet.ipxe"
+  ipxe_script_url  = "https://ipxe-boot-shiftius.apps.us-west-1.starter.openshift-online.com/boot.php?ip=${join("/", [cidrhost(packet_reserved_ip_block.ip_bootstrap.cidr_notation,0), "32"])}&netmask=${packet_reserved_ip_block.ip_bootstrap.netmask}&hostname=${format("${var.cluster_name}-${var.facility}-bootstrap-%02d", count.index)}"
+  // ipxe_script_url  = "https://raw.githubusercontent.com/RedHatSI/openshift-packet-deploy/bootstrap-test/ipxe/rhcos-packet.ipxe"
   plan             = "${var.plan}"
   facilities       = ["${var.facility}"]
   count            = "${var.node_count}"
 
-  billing_cycle = "hourly"
-  project_id    = "${var.project_id}"
+  ip_address {
+     type = "public_ipv4"
+     cidr = 32
+     reservation_ids = [packet_reserved_ip_block.ip_bootstrap.id]
+  }
+  ip_address {
+     type = "private_ipv4"
+  }
+
+  billing_cycle    = "hourly"
+  project_id       = "${var.project_id}"
+  user_data        = "${file("${path.module}/bootstrap.ign")}"
 }
 
 resource "cloudflare_record" "dns_a_bootstrap" {
