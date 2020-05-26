27bd3388672bbfef625d51c0945526a93ffe04b5
JR <liveaverage@gmail.com>
2020-05-26 12:59:24 -0400

    Update output.tf, add wait for openshift-install

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,4 +1,22 @@
- 
+data "external" "kubeadmin_password" {
+  program = ["/bin/bash", "-c", "[ -f \"artifacts/install/auth/kubeadmin-password\" ] && echo \"{\\\"password\\\":\\\"$(cat artifacts/install/auth/kubeadmin-password)\\\"}\""]
+}
+
 output "Information" {
-  value = "Bootstrap node deployed.\n\n"
+  value = <<EOT
+
+
+  OpenShift cluster deployed.
+  Access the OpenShift Web Console at: https://console-openshift-console.apps.${var.cluster_name}.${var.cluster_basedomain}
+
+  Username: kubeadmin
+  Password: ${data.external.kubeadmin_password.result.password}
+
+  To use the CLI:
+    export KUBECONFIG="${abspath(path.root)}/artifacts/install/auth/kubeconfig"
+  
+  Review your nodes:
+    oc get nodes
+
+  EOT
 }
