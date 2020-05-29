4a4780c0a936ce8ceb014ef7ef2163c9bd2bb7d1
J.R. Morgan <liveaverage@users.noreply.github.com>
2020-05-28 21:05:07 -0400

    Update CSR approval resource (#28)
    * Fix CSR approval resource
    
    * Syntax error for CSR approval

--- terraform/modules/install/main.tf
+++ terraform/modules/install/main.tf
@@ -180,7 +180,7 @@ resource "null_resource" "ocp_approve_pending_csrs" {
   command    = <<EOT
     while [ ! -f ${path.root}/artifacts/install/auth/kubeconfig ]; do sleep 2; done;
     sleep 180;
-    export KUBECONFIG=="${path.root}/artifacts/install/auth/kubeconfig";
+    export KUBECONFIG="${path.root}/artifacts/install/auth/kubeconfig";
     export oc=${path.root}/artifacts/oc
     while ($oc get csr | grep -q -i Pending); do echo "Still seeing Pending CSRs"; ($oc get csr -oname | xargs $oc adm certificate approve); sleep 35; done
   EOT
