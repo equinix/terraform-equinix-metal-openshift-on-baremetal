293e1c12f6df482bf107d08f32cef36db7e413a8
vasubabu <kandimalla.vasubabu@gmail.com>
2023-05-08 20:20:15 +0530

    Add provider meta module_name in Equinix Metal TF configs

--- modules/sshkey/versions.tf
+++ modules/sshkey/versions.tf
@@ -2,7 +2,11 @@ terraform {
   required_providers {
     equinix = {
       source = "equinix/equinix"
+      version = "~> 1.14"
     }
   }
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
+  provider_meta "equinix" {
+    module_name = "equinix-metal-openshift-on-baremetal/sshkey"
+  }
 }
